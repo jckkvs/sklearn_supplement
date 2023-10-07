20e5b3e629ba12c06014e034724a1357a4264ad3
jak_tkvs <32125829+jckkvs@users.noreply.github.com>
2023-10-07 20:54:45 +0900

    Update randomprojection.py

--- sklearn_expansion/ensemble/randomprojection.py
+++ sklearn_expansion/ensemble/randomprojection.py
@@ -3,6 +3,95 @@ from sklearn.random_projection import GaussianRandomProjection
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.utils import resample
 import numpy as np
+from joblib import Parallel, delayed
+
+
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.random_projection import GaussianRandomProjection
+from sklearn.utils import resample
+from joblib import Parallel, delayed
+import numpy as np
+from numpy.random import RandomState
+
+class GRPForest(BaseEstimator, RegressorMixin):
+    def __init__(self, n_estimators=10, eps=0.1, 
+                 criterion='squared_error', splitter='best', max_depth=None,
+                 min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0,
+                 max_features=None, random_state=None, max_leaf_nodes=None,
+                 min_impurity_decrease=0.0, ccp_alpha=0.0, n_jobs=1):
+        self.n_estimators = n_estimators
+        self.eps = eps
+        self.criterion = criterion
+        self.splitter = splitter
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
+        self.min_weight_fraction_leaf = min_weight_fraction_leaf
+        self.max_features = max_features
+        self.random_state = random_state
+        self.max_leaf_nodes = max_leaf_nodes
+        self.min_impurity_decrease = min_impurity_decrease
+        self.ccp_alpha = ccp_alpha
+        self.n_jobs = n_jobs
+
+    def _fit_single_tree(self, X, y, sample_weight, random_state):
+        local_rng = RandomState(random_state)        
+        # Initialize GaussianRandomProjection for each tree
+        target_n_components = min(X.shape[1], int(X.shape[1] * (1 - self.eps)))
+        random_projection = GaussianRandomProjection(n_components=target_n_components, eps=self.eps, 
+                                                     random_state=local_rng)
+        
+        if sample_weight is not None:
+            indices = local_rng.choice(range(len(X)), size=len(X), p=sample_weight)
+            new_sample_weight = np.zeros(len(X))
+            for idx in indices:
+                new_sample_weight[idx] += sample_weight[idx]
+            X_bootstrap = X[indices, :]
+            y_bootstrap = y[indices]
+        else:
+            X_bootstrap, y_bootstrap = resample(X, y, random_state=local_rng)
+            new_sample_weight = None
+
+        X_transformed = random_projection.fit_transform(X_bootstrap)
+        model = DecisionTreeRegressor(
+            criterion=self.criterion, splitter=self.splitter,
+            max_depth=self.max_depth, min_samples_split=self.min_samples_split,
+            min_samples_leaf=self.min_samples_leaf, min_weight_fraction_leaf=self.min_weight_fraction_leaf,
+            max_features=self.max_features, random_state=local_rng,
+            max_leaf_nodes=self.max_leaf_nodes, min_impurity_decrease=self.min_impurity_decrease,
+            ccp_alpha=self.ccp_alpha
+        )
+        model.fit(X_transformed, y_bootstrap, sample_weight=new_sample_weight)
+        return model, random_projection
+
+    def fit(self, X, y, sample_weight=None):
+        self.models_ = []
+        self.projections_ = []
+        
+        if self.random_state is not None:
+            rng = RandomState(self.random_state)
+        else:
+            rng = RandomState()
+        
+        random_states = [rng.randint(0, 1 << 30) for _ in range(self.n_estimators)]
+        
+        for random_state in random_states:
+            model, projection = self._fit_single_tree(X, y, sample_weight, random_state)
+            self.models_.append(model)
+            self.projections_.append(projection)
+
+        return self
+    
+    def predict(self, X):
+        predictions = np.zeros(X.shape[0])
+        
+        for model, proj in zip(self.models_, self.projections_):
+            X_transformed = proj.transform(X)  # 射影を適用
+            predictions += model.predict(X_transformed)
+        
+        return predictions / self.n_estimators
+
 
 # Normalize the sample_weight
 def normalize_weights(weights):
